141751403a1d665d18d12090ec9ac52aa86e7e4f
Mariia Moria <mariia.moria@gmail.com>
2021-03-27 13:16:11 -0700

    smoke update and new helpers&testData

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/extended/name.js'
+        './test/smoke/smokeFunctional.js'
     ],
     // Patterns to exclude.
     exclude: [
